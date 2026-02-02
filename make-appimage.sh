2882290b5f3072aab34689ac18b25ad153000cc8
Link4Electronics <link4electronics@gmail.com>
2026-02-02 07:18:51 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,6 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/pixmaps/eduke32.ico
-#export DESKTOP=eduke32.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
